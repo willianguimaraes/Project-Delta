3aa24888046d0a53ff7b48cd091e607df66630fc
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-06-10 09:54:34 -0300

    Projeto criado com Spring Boot

---
+++ src/main/java/com/kohatsu/projectdelta/ProjectDeltaApplication.java
@@ -0,0 +1,12 @@
+package com.kohatsu.projectdelta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDeltaApplication.class, args);
+	}
+}
